0d92c83f78e27647b0be5712c79ab93a47865194
SuperGandhi <patrickborjabueno@hotmail.com>
2022-12-30 15:15:21 -0500

    Complete

--- firts_project.py
+++ firts_project.py
@@ -8,6 +8,8 @@ print("The name of your beer\nis '" + input("What city like meet?: ") + " " + in
 print("The name of your beer\nis '" + input("What city like meet?: ") + " " + input("What's your favorite color?: ") + "'\nCongratulations!")
 
 
+print("The name of your beer\nis '" + input("What city like meet?: ") + " " + input("What's your favorite color?: ") + "'\nCongratulations!")
+
 
 
 

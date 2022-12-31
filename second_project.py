aaa6cdeb7882fee2a6bb523533dc631533c7cb9a
SuperGandhi <patrickborjabueno@hotmail.com>
2022-12-31 17:52:41 -0500

    Complete

--- second_project.py
+++ second_project.py
@@ -1,7 +1,7 @@
 
 user = input("What's your name : ")
 sales = float(input("How much have they sold per month : "))
-salary =  sales * 13 / 100
+salary =  round(sales * 13 / 100)
 
 
 print(f"Ok, {user}. This month you won {salary}$")

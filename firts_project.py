e53867539e46795890e9281a448e74a54483e002
SuperGandhi <patrickborjabueno@hotmail.com>
2022-12-31 17:48:09 -0500

    Complete

--- firts_project.py
+++ firts_project.py
@@ -2,14 +2,6 @@
 print("The name of your beer\nis '" + input("What city like meet?: ") + " " + input("What's your favorite color?: ") + "'\nCongratulations!")
 
 
-print("The name of your beer\nis '" + input("What city like meet?: ") + " " + input("What's your favorite color?: ") + "'\nCongratulations!")
-
-
-print("The name of your beer\nis '" + input("What city like meet?: ") + " " + input("What's your favorite color?: ") + "'\nCongratulations!")
-
-
-print("The name of your beer\nis '" + input("What city like meet?: ") + " " + input("What's your favorite color?: ") + "'\nCongratulations!")
-
 
 
 

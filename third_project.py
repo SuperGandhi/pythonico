6b6fb5491e98fa2ce4b35b11b066880dda3012a1
SuperGandhi <patrickborjabueno@hotmail.com>
2023-01-21 23:53:55 -0500

    Add solution

--- third_project.py
+++ third_project.py
@@ -13,3 +13,20 @@ print(fragment)
 text_fragment = "count( ) : retorna el número de veces que se repite un conjunto de caracteres especificado"
 
 print(text_fragment.count("número"))
+
+
+# line_string = "Coming soon"
+# result = line_string.index("o", 3)
+# print(result)
+
+
+text = "ABCDFGHIJKLMNOPQRSTUVWXYZ"
+              #[start,end,secuence]
+fragment = text[2:5:2]
+               #CF 
+fragment = text[::-1] # inverse order
+print(fragment)
+
+text_fragment = "count( ) : retorna el número de veces que se repite un conjunto de caracteres especificado"
+
+print(text_fragment.count("número"))

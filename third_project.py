be911a2a1526a92e2bc44816a2f2095d288f5174
SuperGandhi <patrickborjabueno@hotmail.com>
2023-01-24 22:27:40 -0500

    Complete

--- third_project.py
+++ third_project.py
@@ -16,16 +16,7 @@ print(text_fragment.count("número"))
 
 
 
-text = "ABCDFGHIJKLMNOPQRSTUVWXYZ"
-              #[start,end,secuence]
-fragment = text[2:5:2]
-               #CF 
-fragment = text[::-1] # inverse order
-print(fragment)
 
-text_fragment = "count( ) : retorna el número de veces que se repite un conjunto de caracteres especificado"
-
-print(text_fragment.count("número"))
 
 
 

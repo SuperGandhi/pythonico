98900a08a15a448f07e06ac4dd5a94cb9ea9ba1c
SuperGandhi <patrickborjabueno@hotmail.com>
2023-02-15 18:50:09 -0500

    Update change

---
+++ unit_three/substring_practice.py
@@ -0,0 +1,11 @@
+#  Extrae la primera palabra de la siguiente frase utilizando slicing, y muéstrala en pantalla
+phrase_one = "Controlar la complejidad es la esencia de la programación"
+print(phrase_one[0:10])
+
+# Toma cada tercer caracter empezando desde el noveno hasta el final de la frase, e imprime el resultado
+phrase_two = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+print(phrase_one[10:-1:3])
+
+# Invierte la posición de todos los caracteres de la siguiente frase y muestra el resultado en pantalla
+phrase_three = "Nunca confíes en un ordenador que no puedas lanzar por una ventana"
+print(phrase_one[::-1])

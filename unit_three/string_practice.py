36ba32f70667f402f20bcb7278acbe950dd26366
SuperGandhi <patrickborjabueno@hotmail.com>
2023-02-28 14:04:26 -0500

    Complete

--- unit_three/string_practice.py
+++ unit_three/string_practice.py
@@ -4,3 +4,14 @@ Concatena 15 veces el texto "Repetición" y muestra el resultado en pantalla. Po
 """
 rep_string = "Repetición"
 print(rep_string*15)
+
+
+
+"""
+Práctica Propiedades de Strings 2 Verifica si la palabra "agua" no se encuentra en el siguiente haiku. Debes imprimir el booleano. Tierra mojada, mis recuerdos de viaje entre las lluvias Tierra mojada mis recuerdos de viaje, entre las lluvias
+
+"""
+haiku = "Tierra mojada, mis recuerdos de viaje entre las lluvias Tierra mojada mis recuerdos de viaje, entre las lluvias"
+
+print(haiku in "agua")
+print(haiku not in "agua")

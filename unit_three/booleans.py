debc589aa0fe701c754e0eac13dac424933494f2
SuperGandhi <patrickborjabueno@hotmail.com>
2023-03-04 17:43:11 -0500

    Complete

---
+++ unit_three/booleans.py
@@ -0,0 +1,27 @@
+import math
+
+"""
+Práctica Booleanos 1 Realiza una comparación que arroje como resultado un booleano y almacena el resultado (True/False) en una variable llamada prueba
+
+"""
+firts_element = True
+second_element = 3
+prueba = print(firts_element == second_element)
+
+"""
+
+Práctica Booleanos 2 Verifica si 17834/34 es mayor que 87*56 y muestra el resultado (booleano) en pantalla utilizando print()
+
+"""
+values = 17834 / 34 > 87 * 56
+print(values)
+
+
+"""
+
+Práctica Booleanos 3 Verifica si la raíz cuadrada de 25 es igual a 5 y muestra el resultado (booleano) en pantalla utilizando print()
+
+"""
+num = 25
+result = math.sqrt(num) == 5
+print(result)

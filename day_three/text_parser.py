7d9a415988263704d66b9044f73962afed7da86d
SuperGandhi <patrickborjabueno@hotmail.com>
2023-03-05 16:39:22 -0500

    Advance

---
+++ day_three/text_parser.py
@@ -0,0 +1,8 @@
+text_user = input('Entry your text: ')
+letters_user = []
+text = text.lower()
+
+letters_user.append(input('Enter the firts letter: ')) 
+letters_user.append(input('Enter the second letter: ')) 
+letters_user.append(input('Enter the third letter: ')) 
+

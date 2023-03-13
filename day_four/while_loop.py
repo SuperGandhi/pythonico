7c76fe83653d527094ea5fcc72f56b6a522d2aab
SuperGandhi <patrickborjabueno@hotmail.com>
2023-03-13 09:29:22 -0500

    Complete third exercise

--- day_four/while_loop.py
+++ day_four/while_loop.py
@@ -20,3 +20,20 @@ while numero > 0:
    numero -= 1
    if numero % 5 == 0:
       print("¡recuerda que aquí puedes utilizar la operación módulo dividiendo por 5 y verificando el resto¡")
+
+
+
+"""
+   Práctica Loop While 3 Crea un loop For a lo largo de la siguiente lista de números, imprimiendo en pantalla cada uno de sus elementos, e interrumpe el flujo en el momento que encuentres un valor negativo: lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3] No debes cambiar el orden de la lista. 
+
+"""
+
+lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
+
+for lista in lista_numeros:
+   if lista < 0:
+      break
+   print(lista)
+
+
+

5294c993f0f861c8b6961bf19f85cff319cc5b91
SuperGandhi <patrickborjabueno@hotmail.com>
2023-03-13 09:20:42 -0500

    Exercise two complete

--- day_four/while_loop.py
+++ day_four/while_loop.py
@@ -8,3 +8,15 @@ numero = 11
 while numero > 0:
    numero -= 1
    print(numero)
+
+"""
+   Práctica Loop While 2 Crea un Loop While que reste de uno en uno los números desde el 50 al 0 (ambos números incluídos) con las siguientes condiciones adicionales: - Si el número es divisible por 5, mostrar dicho número en pantalla (¡recuerda que aquí puedes utilizar la operación módulo dividiendo por 5 y verificando el resto!) - Si el número no es divisible por 5, continuar ejecutando el loop sin mostrar el valor en pantalla (no te olvides de seguir restando para que el programa no corra infinitamente). numero = 50
+
+"""
+
+numero = 51
+
+while numero > 0:
+   numero -= 1
+   if numero % 5 == 0:
+      print("¡recuerda que aquí puedes utilizar la operación módulo dividiendo por 5 y verificando el resto¡")

389cc0eb827f7f7094528598c145ef7fbdd99306
SuperGandhi <patrickborjabueno@hotmail.com>
2023-03-13 09:15:14 -0500

    Firts exercise complete

---
+++ day_four/while_loop.py
@@ -0,0 +1,10 @@
+"""
+   Práctica Loop While 1 Crea un Loop While que se imprima en pantalla los números del 10 al 0, uno a la vez. numero = 10
+
+"""
+
+numero = 11
+
+while numero > 0:
+   numero -= 1
+   print(numero)

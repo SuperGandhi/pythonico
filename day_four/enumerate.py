81e7fdd3e1a6f9d90730ba38bbd206dcbc21463a
SuperGandhi <patrickborjabueno@hotmail.com>
2023-03-22 04:58:13 -0500

    Exercise complete

--- day_four/enumerate.py
+++ day_four/enumerate.py
@@ -19,11 +19,14 @@ lista_indices = "Python"
 for indice,elemento in enumerate(lista_indices):
    print(f'{indice} -> {elemento}')    
 
-
-
-
 """
 
    Práctica Enumerador 3 Imprime en pantalla únicamente los índices de aquellos nombres de la lista a continuación, que empiecen con M: lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"] Puedes resolverlo de diferentes maneras, pero servirá que tengas presente todos o algunos de los siguientes elementos: Loops Condicionales if El método enumerate() Métodos de strings o indexado lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
 
 """
+
+lista_nombres = ["Marcos", "Laura", "Mónica", "Javier", "Celina", "Marta", "Darío", "Emiliano", "Melisa"]
+
+for index,nombres in enumerate(lista_nombres):
+    if nombre.startswith("M"):
+       print(f'Only the next names in the index ubication empty with M : {index}')

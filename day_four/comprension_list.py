7de438d35aa7c16c66e0ee3a6c0762c3a383ada4
SuperGandhi <patrickborjabueno@hotmail.com>
2023-03-24 16:46:14 -0500

    Exercise two complete

--- day_four/comprension_list.py
+++ day_four/comprension_list.py
@@ -1,5 +1,5 @@
 """
-   Práctica Comprensión de Listas 1 Para realizar el ejercicio a continuación, puedes optar por diferentes caminos. Si bien en programación el camino correcto es el que devuelve el resultado correcto, te animo a que intentes aplicar los conceptos de comprensión de listas para comenzar a afianzarlos para el futuro. ¡Pueden resultarte muy útiles en tu práctica profesional! Crea una lista valores_cuadrado formada por los números de la lista valores, elevados al cuadrado. valores = [1, 2, 3, 4, 5, 6, 9.5] valores = [1, 2, 3, 4, 5, 6, 9.5]
+   Práctica Comprensión de Listas 1 Para realizar el ejercicio a continuación, puedes optar por diferentes caminos. Si bien en programación el camino correcto es el que devuelve el resultado correcto, te animo a que intentes aplicar los conceptos de comprensión de listas para comenzar a afianzarlos para el futuro. ¡Pueden resultarte muy útiles en tu práctica profesional! Crea una lista valores_cuadrado formada por los números de la lista valores, elevados al cuadrado. valores = [1, 2, 3, 4, 5, 6, 9.5] 
 
 """
 
@@ -9,13 +9,14 @@ print(valores_cuadradados)
 
 
 """
-   Práctica Comprensión de Listas 2 Para realizar el ejercicio a continuación, puedes optar por diferentes caminos. Si bien en programación el camino correcto es el que devuelve el resultado correcto, te animo a que intentes aplicar los conceptos de comprensión de listas para comenzar a afianzarlos para el futuro. ¡Pueden resultarte muy útiles en tu práctica profesional! Crea una lista valores_pares formada por los números de la lista valores que (¡adivinaste!) sean pares. valores = [1, 2, 3, 4, 5, 6, 9.5] valores = [1, 2, 3, 4, 5, 6, 9.5]
+   Práctica Comprensión de Listas 2 Para realizar el ejercicio a continuación, puedes optar por diferentes caminos. Si bien en programación el camino correcto es el que devuelve el resultado correcto, te animo a que intentes aplicar los conceptos de comprensión de listas para comenzar a afianzarlos para el futuro. ¡Pueden resultarte muy útiles en tu práctica profesional! Crea una lista valores_pares formada por los números de la lista valores que (¡adivinaste!) sean pares. valores = [1, 2, 3, 4, 5, 6, 9.5] 
 
 """
 
 
-
-
+valores = [1, 2, 3, 4, 5, 6, 9.5]
+valores_pares = [valor_par for valor_par in valores if valor_par % 2 == 0]
+print(valores_pares)
 
 
 

96961cc4af3a68e2534334bb4b1d041249254fa5
SuperGandhi <patrickborjabueno@hotmail.com>
2023-04-25 23:50:57 -0500

    Upload exercises

--- day_five/functions_intertaction.py
+++ day_five/functions_intertaction.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint,choice,
 
 """
 Práctica sobre Interacción entre Funciones 1
@@ -18,5 +18,68 @@ Si la suma es mayor o igual a 10:
 
 "La suma de tus dados es {suma_dados}. Parece una jugada ganadora"
 
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Práctica sobre Interacción entre Funciones 2
+Crea una función llamada reducir_lista() que tome una lista (lista_numeros) como argumento, y devuelva la misma lista, pero eliminando duplicados (dejando un solo número si hay repetidos) y eliminando el valor más alto. El orden de los elementos puede modificarse.
+
+Por ejemplo, si se le proporciona la lista [1,2,15,7,2] debe devolver [1,2,7].
+
+Emplea la lista devuelta por la primera función y calcula el promedio de los valores de sus elementos, creando una nueva función llamada promedio().
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Práctica sobre Interacción entre Funciones 3
+Crea una función (llamada lanzar_moneda) que devuelva el resultado de lanzar una moneda (al azar). Dicha función debe poder devolver los resultados "Cara" o "Cruz", y no debe recibir argumentos para funcionar.
+
+Crea una segunda función (llamada probar_suerte), que tome dos argumentos: el primero, debe ser el resultado del lanzamiento de la moneda. El segundo argumento, será una lista de números cualquiera (lista_numeros).
+
+Si se le proporciona una "Cara", debe mostrar el mensaje al usuario: "La lista se autodestruirá", y eliminarla (devolverla como lista vacía []).
+
+Si se le proporciona una "Cruz", debe imprimir en pantalla: "La lista fue salvada" y devolver la lista intacta.
 
 """

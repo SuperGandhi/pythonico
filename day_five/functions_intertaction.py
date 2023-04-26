55e4145685a5e7796ebee381972d614b056a3596
SuperGandhi <patrickborjabueno@hotmail.com>
2023-04-25 23:44:21 -0500

    Add exercise and random biblio

---
+++ day_five/functions_intertaction.py
@@ -0,0 +1,22 @@
+from random import randint
+
+"""
+Práctica sobre Interacción entre Funciones 1
+Crea una función (lanzar_dados) que arroje dos dados al azar y devuelva sus resultados (la función debe retornar dos valores resultado, que se encuentren entre 1 y 6).
+
+Proporciona el resultado de estos dos dados a una función que se llame evaluar_jugada (es decir, esta segunda función debe recibir dos argumentos) y que retorne un mensaje según la suma de estos valores:
+
+Si la suma es menor o igual a 6:
+
+"La suma de tus dados es {suma_dados}. Lamentable"
+
+Si la suma es mayor a 6 y menor a 10:
+
+"La suma de tus dados es {suma_dados}. Tienes buenas chances"
+
+Si la suma es mayor o igual a 10:
+
+"La suma de tus dados es {suma_dados}. Parece una jugada ganadora"
+
+
+"""

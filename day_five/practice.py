a216d49eea8b9cd86f783369053b9660074bda86
SuperGandhi <patrickborjabueno@hotmail.com>
2023-05-19 11:01:53 -0500

    Complete exercises

--- day_five/practice.py
+++ day_five/practice.py
@@ -22,6 +22,9 @@ print(frutas)
 
 
 """
-Práctica Métodos y Ayuda 3 Verifica si los sets a continuación forman conjuntos aislados (es decir, que no tienen elementos en común), utilizando el método isdisjoint(). Almacena dicho resultado en la variable conjuntos_aislados: marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"} marcas_tv = {"Sony", "Philips", "Samsung", "LG"} Busca en la documentación acerca del funcionamiento del método solicitado para saber cómo actúa y cómo es su funcionamiento. marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"} marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
-
+Práctica Métodos y Ayuda 3 Verifica si los sets a continuación forman conjuntos aislados (es decir, que no tienen elementos en común), utilizando el método isdisjoint(). Almacena dicho resultado en la variable conjuntos_aislados: marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"} marcas_tv = {"Sony", "Philips", "Samsung", "LG"} 
 """
+marcas_smartphones = {"Samsung", "Xiaomi", "Apple", "Huawei", "LG"}
+marcas_tv = {"Sony", "Philips", "Samsung", "LG"}
+brands_joint = marcas_smartphones.isdisjoint(marcas_tv)
+print(brands_joint)

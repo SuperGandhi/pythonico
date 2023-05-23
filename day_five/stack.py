a6eefab0e0ee03f214a168c063d530eb64075590
SuperGandhi <patrickborjabueno@hotmail.com>
2023-05-23 10:29:02 -0500

    Add stack

---
+++ day_five/stack.py
@@ -0,0 +1,25 @@
+"""
+    Implemented stack with procedimental programming
+    
+"""
+stack = []
+
+
+def push(val):
+    stack.append(val)
+
+
+def pop(val):
+    val = stack[-1]
+    del stack[-1]
+    return val 
+
+
+push(3)
+push(4)
+push(10)
+
+print(pop())
+print(pop())
+print(pop())
+    

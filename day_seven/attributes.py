d6d90a82edc02b2b1d36dcecf5eddefa49daf7f4
SuperGandhi <patrickborjabueno@hotmail.com>
2023-06-03 15:43:22 -0500

    Add exercises

---
+++ day_seven/attributes.py
@@ -0,0 +1,58 @@
+"""
+    Crea una clase llamada Casa, y asígnale atributos: color, cantidad_pisos.
+
+    Crea una instancia de Casa, llamada casa_blanca, de color "blanco" y cantidad de pisos igual a 4.
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+    Crea una clase llamada Cubo, y asígnale el atributo de clase:
+
+    caras = 6
+
+    y el atributo de instancia:
+
+    color
+
+    Crea una instancia cubo_rojo, de dicho color.
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+    Crea una clase llamada Personaje, y asígnale el siguiente atributo de clase:
+
+    real = False
+
+    Crea una instancia llamada harry_potter con los siguientes atributos de instancia:
+
+    especie = "Humano"
+
+    magico = True
+
+    edad = 17
+
+"""

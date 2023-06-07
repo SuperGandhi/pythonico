639d5b24f122db1312cc57b8009dd05f39e79aae
SuperGandhi <patrickborjabueno@hotmail.com>
2023-06-07 11:50:24 -0500

    Add documentation

---
+++ day_seven/seven_project.py
@@ -0,0 +1,18 @@
+
+"""
+    Cuenta bancaria
+        crear class Persona
+            atributos: nombre y apellido
+        crear clase Cliente(Persona)
+            atributos cliente: numero_cuenta y balance
+            métodos :
+                    imprimir los datos del cliente
+                    depositar cuanto dinero quiere agregar a su cuenta
+                    retirar cuanto dinero quiere sacar de su cuenta
+        código para que el usuario elija si quiere Depositar,Retirar o Salir
+            funciones:
+                    crear_cliente()
+                    inicio()
+    
+
+"""

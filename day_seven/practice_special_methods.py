3f5f527c906ffbf2a649e1e45a52074a32397f71
SuperGandhi <patrickborjabueno@hotmail.com>
2023-06-07 11:39:51 -0500

    Solve exercises

--- day_seven/practice_special_methods.py
+++ day_seven/practice_special_methods.py
@@ -12,18 +12,18 @@ class Libro():
         self.cantidad_paginas = cantidad_paginas
 
 
-    def completa(self):
-        pass
+    def __str__(self):
+        return f'"{self.titulo}" de {self.autor}'
     
-    
+# Ejemplo de uso
+la_republica = Libro("La república", "Platón", 500)
+print(la_republica)
 
 
 """
     Dada la clase Libro, implementa el método especial __len__ para que cada vez que se ejecute la función len() sobre el mismo, devuelva el número de páginas como número entero.
 
 """
-
-
 
 
 class Libro():
@@ -33,10 +33,14 @@ class Libro():
         self.autor = autor
         self.cantidad_paginas = cantidad_paginas
 
-    def completar(self):
-        pass
+    def __len__(self):
+        return self.cantidad_paginas
     
-    
+
+# Ejemplo de uso
+la_rama_dorada = Libro("La rama dorada", "Frazer", 1000)
+print(len(la_rama_dorada))
+
 
 """
     Dada la clase Libro, implementa el método especial __del__ para que el usuario sea informado con el mensaje "Libro eliminado", mostrándolo en pantalla cada vez que el libro se elimine.
@@ -51,5 +55,10 @@ class Libro():
         self.autor = autor
         self.cantidad_paginas = cantidad_paginas
 
-    def completar(self):
-        pass
+    def __del__(self):
+        return "Libro eliminado"
+
+
+# Ejemplo de uso
+libro = Libro("El Gran Gatsby", "F. Scott Fitzgerald", 500)
+del libro  # Al eliminar el libro, se mostrará "Libro eliminado" en pantalla

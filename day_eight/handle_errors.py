8d9731cb54af364e53074f8f283b6bad25095401
SuperGandhi <patrickborjabueno@hotmail.com>
2023-06-12 10:17:37 -0500

    Add exercises

---
+++ day_eight/handle_errors.py
@@ -0,0 +1,61 @@
+
+"""
+    Implementa para la siguiente función suma(), un manejador de errores simple que ante cualquier error, imprima en pantalla el mensaje: "Error inesperado". En caso contrario, deberá limitarse a mostrar el resultado de la suma entre los dos números.
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+    Implementa para la siguiente función cociente(), un manejador de errores:
+
+    Ante un error de tipo (TypeError), debe imprimir en pantalla el mensaje: "Los argumentos a ingresar deben ser números"
+
+    Si se generara una división por cero (error del tipo ZeroDivisionError), el mensaje mostrado debe ser: "El segundo argumento no debe ser cero"
+
+    En caso que no se produzca un error, deberá limitarse a imprimir el resultado del cociente (división) entre los dos números entregados como argumento.
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+    Implementa un manejador de errores dentro de la siguiente función, abrir_archivo():
+
+    En caso de que el archivo que se intenta abrir no pueda ser hallado (FileNotFoundError), mostrar en pantalla el mensaje: "El archivo no fue encontrado"
+
+    En caso de que otro tipo de error ocurra, mostrar el mensaje: "Error desconocido"
+
+    Si no se produce ningún error, imprimir en pantalla: "Abriendo exitosamente"
+
+    En todos los casos, al finalizar, imprimir: "Finalizando ejecución"
+
+
+"""

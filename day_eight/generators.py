4cf988ca49a4d548766512bb0231dd430684c623
SuperGandhi <patrickborjabueno@hotmail.com>
2023-06-21 09:29:45 -0500

    Add project

--- day_eight/generators.py
+++ day_eight/generators.py
@@ -18,14 +18,10 @@ print(next(generator))
 print(next(generator))
 
 
-
-
 """
     Crea un generador (almacenado en la variable generador) que sea capaz de devolver de manera indefinida múltiplos de 7, iniciando desde el mismo 7, y que cada vez que sea llamado devuelva el siguiente múltiplo (7, 14, 21, 28...).
 
 """
-
-
 
 
 

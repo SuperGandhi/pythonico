5bf662af2ff55c1a26a2cecce65a048bae4e9939
supergandhi <patrickborjabueno@hotmail.com>
2023-08-04 15:39:23 -0500

    Add variable instance

---
+++ day_seven/variables.py
@@ -0,0 +1,30 @@
+
+# Variables de instancia
+
+class Financial:
+    
+    def __init__(self,assets,pasive):
+        self.assets = assets
+        self.pasive = pasive
+        self.incomes = 0 # variable de instancia 
+
+
+    def incomes_plus(self, plus):
+        self.incomes += plus
+    
+    def bills(self, less):
+        self.incomes -= less
+        
+
+
+julius = Financial(30,20) 
+
+julius.incomes_plus(30)
+julius.bills(50)
+
+print(julius.assets , julius.pasive, julius.incomes)
+        
+
+
+
+# Variables de clase

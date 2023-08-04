249daf5bc48b36145c9ea408f8025d03a230dd63
supergandhi <patrickborjabueno@hotmail.com>
2023-08-04 15:43:28 -0500

    Add variable class

--- day_seven/variables.py
+++ day_seven/variables.py
@@ -25,6 +25,30 @@ julius.bills(50)
 print(julius.assets , julius.pasive, julius.incomes)
         
 
-
-
 # Variables de clase
+
+
+class Financial:
+    
+    incomes = 0 # variable de clase
+        
+    def __init__(self,assets,pasive):
+        self.assets = assets
+        self.pasive = pasive
+        self.incomes = 0 # variable de instancia 
+
+
+    def incomes_plus(self, plus):
+        self.incomes += plus
+    
+    def bills(self, less):
+        self.incomes -= less
+        
+
+
+julius = Financial(30,20) 
+
+julius.incomes_plus(30)
+julius.bills(50)
+
+print(julius.assets , julius.pasive, julius.incomes)

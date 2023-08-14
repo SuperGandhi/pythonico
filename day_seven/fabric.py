bc89026bb1dbaa448cccb3b383c4562656d1b7ee
supergandhi <patrickborjabueno@hotmail.com>
2023-08-13 20:31:41 -0500

    Complete fabric

--- day_seven/fabric.py
+++ day_seven/fabric.py
@@ -7,10 +7,36 @@ class Fabrica:
     
 
 class Moto(Fabrica):
-    pass
+    
+    def data(self):
+        print("La cantidad de llantas es de: ", self.llantas)
+        print("El color de la moto es: ", self.color)
+        print("El precio de la moto es: ", self.precio)
 
 class Carro(Fabrica):
-    pass
+    
+        def data(self):
+            print("La cantidad de llantas es de: ", self.llantas)
+            print("La color del carro es: ", self.color)
+            print("El precio del carro es: ", self.precio)
+        
+
+moto = Moto(2,"verde",5000)
+moto.data()
+carro =  Carro(4,"negro", 2000)
+carro.data()
+
+
+
+
+
+
+
+
+
+
+
+    
 
 
         

e4368aef655d6662ffd3509370ebd8820e64bb37
supergandhi <patrickborjabueno@hotmail.com>
2023-08-16 21:42:10 -0500

    Add decorator

--- day_eight/example_decorators.py
+++ day_eight/example_decorators.py
@@ -1,12 +1,26 @@
-def decorar_saludo(funcion):
+# def decorar_saludo(funcion):
     
     
-    def otra_funcion(palabra):
-        print("Hi")
-        funcion(palabra)
-        print("Bye")
-    return otra_funcion
+#     def otra_funcion(palabra):
+#         print("Hi")
+#         funcion(palabra)
+#         print("Bye")
+#     return otra_funcion
 
-@decorar_saludo
-def mayusculas(text):
-    pass
+
+
+# @decorar_saludo
+# def mayusculas(text):
+#     return text.upper()
+
+
+# def minusculas(text):
+#     return text.lower()
+
+# mayuscula_decorada = decorar_saludo(mayusculas)
+# mayuscula_decorada("hola")
+
+class Casa:
+
+	def __init__(self, precio):
+		self.__precio = precio
